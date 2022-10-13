e38a4b62084725409c87ddc51f9974d56af32044
frol_cao <nkita_0404@mail.ru>
2022-10-13 18:31:45 +0300

    Seminar 8

---
+++ task1/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+int[,] generate2DArray(int height, int width, int start, int end)
+{
+    int[,] twoDArray = new int[height, width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            twoDArray[i,j] = new Random().Next(start, end + 1);
+        }
+    }
+    return twoDArray;
+}
+
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void printArray(int[,] arrayToPrint)
+{
+    Console.Write("\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        printColorData(i+"\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        printColorData(i+"\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ordered2DArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inputArray.GetLength(1) - 1; k++)
+            {
+                if (inputArray[i, k] < inputArray[i, k + 1])
+                {
+                    int temp = inputArray[i, k + 1];
+                    inputArray[i, k + 1] = inputArray[i, k];
+                    inputArray[i, k] = temp;
+                } 
+            }
+        }    
+    }
+}
+
+int[,] generetedArray = generate2DArray(5, 5, -100, 100);
+printArray(generetedArray);
+
+ordered2DArray(generetedArray);
+printArray(generetedArray);
---
+++ task2/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+ Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+
+
+*/
+
+int[,] generate2DArray(int height, int width, int start, int end)
+{
+    int[,] twoDArray = new int[height, width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            twoDArray[i,j] = new Random().Next(start, end + 1);
+        }
+    }
+    return twoDArray;
+}
+
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void printArray(int[,] arrayToPrint)
+{
+    Console.Write("\t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        printColorData(i+"\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        printColorData(i+"\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] resultMatrix(int[,] firstMatrix, int[,] secondMatrix)
+{
+    int[,] newMatrix = new int[firstMatrix.GetLength(0), secondMatrix.GetLength(1)];
+    for (int i = 0; i < firstMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < secondMatrix.GetLength(0); j++)
+        {
+            for (int k = 0; k < secondMatrix.GetLength(1); k++)
+            {
+                newMatrix[i, k] += firstMatrix[i, j] * secondMatrix[j, k];
+            }
+        }    
+    }
+    return newMatrix;
+}
+
+int[,] generetedArray1 = generate2DArray(2, 2, -100, 100);
+printArray(generetedArray1);
+int[,] generetedArray2 = generate2DArray(2, 2, -100, 100);
+printArray(generetedArray2);
+
+int[,] outcomingMatrix = resultMatrix(generetedArray1, generetedArray2);
+printArray(outcomingMatrix);
